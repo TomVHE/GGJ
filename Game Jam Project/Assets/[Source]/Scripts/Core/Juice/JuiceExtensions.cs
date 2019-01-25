dd38693a55624b43fb0f057bba96cf9403d11173
Walter Haynes <walter-haynes@outlook.com>
2019-01-25 22:55:45 +0100

    Added packages + Updated to 2018.3.3f1

---
+++ Game Jam Project/Assets/[Source]/Scripts/Core/Juice/JuiceExtensions.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public static class JuiceExtensions
+{
+	public static void Shake (this Camera cam)
+	{
+		Screenshake shake = cam.transform.GetComponent<Screenshake>();
+		if(shake != null)
+		{
+			shake.Shake();
+		}
+		else
+		{
+			Debug.Log("Camera doesn't have Screenshake component, adding one!");
+			cam.transform.gameObject.AddComponent<Screenshake>().Shake(); //add and shake
+		}
+	}
+	public static void Shake (this Camera cam, float multi)
+	{
+		Screenshake shake = cam.transform.GetComponent<Screenshake>();
+		if(shake != null)
+		{
+			shake.Shake(multi);
+		}
+		else
+		{
+			Debug.Log("Camera doesn't have Screenshake component, adding one!");
+			cam.transform.gameObject.AddComponent<Screenshake>().Shake(multi); //add and shake
+		}
+	}
+	
+	public static void Kick (this Camera cam, Vector3 dir)
+	{
+		Kickback kick = cam.transform.GetComponent<Kickback>();
+		if(kick != null)
+		{
+			kick.Kick(dir);
+		}
+		else
+		{
+			Debug.Log("Camera doesn't have Kickback component, adding one!");
+			cam.transform.gameObject.AddComponent<Kickback>().Kick(dir); //add and shake
+		}
+	}
+	public static void Kick (this Camera cam, Vector3 dir, float multi)
+	{
+		Kickback kick = cam.transform.GetComponent<Kickback>();
+		if(kick != null)
+		{
+			kick.Kick(dir, multi);
+		}
+		else
+		{
+			Debug.Log("Camera doesn't have Kickback component, adding one!");
+			cam.transform.gameObject.AddComponent<Kickback>().Kick(dir, multi); //add and shake
+		}
+	}
+}
---
+++ Game Jam Project/Assets/[Source]/Scripts/Core/Movement/VehicleInput.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class VehicleInput : MonoBehaviour
+{
+    //private float thrust;
+    //private float steer;				
+    //private bool isDrifting;
+
+    public float Thrust { get; protected set; }
+    public float Steer { get; protected set; }
+    public bool IsDrifting { get; protected set; }
+
+    public bool stuntingSameAsDrifting = true;
+
+    public bool StuntingSameAsDrifting
+    {
+        get { return stuntingSameAsDrifting; }
+    }
+
+    private bool isStunting;
+    public bool IsStunting
+    {
+        get
+        {
+            if (StuntingSameAsDrifting)
+            {
+                return IsDrifting;
+            }
+            else
+            {
+                return isStunting;
+            }
+        }
+        protected set
+        {
+            if (StuntingSameAsDrifting)
+            {
+                IsDrifting = value;
+            }
+            else
+            {
+                isStunting = value;
+            }
+        }
+    }
+}
