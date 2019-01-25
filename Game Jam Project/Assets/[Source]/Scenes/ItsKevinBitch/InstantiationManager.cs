c97823d58bc9f292b90e7b683eba8e17cb6dcb83
Kevin Otten <kevin.otten@ziggo.nl>
2019-01-25 23:43:21 +0100

    start

---
+++ Game Jam Project/Assets/[Source]/Scenes/ItsKevinBitch/InstantiationManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class InstantiationManager : SerializedMonoBehaviour
+{
+   public Dictionary<string, GameObject> objectss = new Dictionary<string, GameObject>();
+
+   public void InstantiateMonsters()
+   {
+      
+   }
+}
