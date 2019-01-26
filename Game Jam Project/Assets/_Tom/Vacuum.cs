f25f925738a2a05a997becd05e753d1774c287e5
Tomvhe <tvhe@outlook.com>
2019-01-26 13:36:08 +0100

    Added Vacuum effect

---
+++ Game Jam Project/Assets/_Tom/Vacuum.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vacuum : MonoBehaviour
+{
+    [SerializeField] private float pullForce = 300f;
+    
+    private Vector3 forceDirection;
+    private Rigidbody rb;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if (other.CompareTag("Letter"))
+            {
+                forceDirection = transform.position - other.transform.position;
+
+                rb = other.transform.GetComponent<Rigidbody>();
+
+                rb.AddForce(forceDirection.normalized * pullForce * Time.deltaTime);
+            }
+        }
+    }
+}
