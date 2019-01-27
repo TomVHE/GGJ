5680a8b56bf25cb937ea55d267f0bfd2f966783c
Avonexile <arnevanommen@outlook.com>
2019-01-27 12:58:29 +0100

    lookat script

---
+++ Game Jam Project/Assets/_Arne/_Scripts/LookAt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    private Transform camTransform;
+    	
+	// Use this for initialization
+	void Start () 
+	{	
+		camTransform = Camera.main.transform;
+	}
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+        Vector3 targetPostition = new Vector3(camTransform.position.x, this.transform.position.y, camTransform.position.z);
+        this.transform.LookAt(targetPostition);
+	}
+}
