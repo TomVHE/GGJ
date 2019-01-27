bea73a812810def8d79bfbc56ff4bde36854acd0
Avonexile <arnevanommen@outlook.com>
2019-01-27 14:34:16 +0100

    Particles activated

--- Game Jam Project/Assets/KillZone.cs
+++ Game Jam Project/Assets/KillZone.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class KillZone : MonoBehaviour
 {
+    public ParticleActive activateParticle;
+
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Letter"))
         {
+            activateParticle.Activate();
             other.transform.GetComponent<Letter>().Death();
         }
     }
---
+++ Game Jam Project/Assets/_Arne/_Scripts/ParticleActive.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleActive : MonoBehaviour
+{
+    public ParticleSystem particle;
+    
+    public void Activate()
+    {
+        particle.Play();
+    }
+}
